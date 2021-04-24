e7544b4d656c1e563dc33879bf237f62b79b7bba
Michael Hall <michaelhall@michaels-mini.home>
2021-04-23 23:19:36 -0400

    Fix cache busting not working for app.css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,7 +19,7 @@
                 font-family: fieldwork, sans-serif;;
             }
         </style>
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
