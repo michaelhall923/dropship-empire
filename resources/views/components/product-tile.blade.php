dbd704bfef74448d36d1cc9a7ce097f7fd024417
Michael Hall <michaelhall@michaels-mini.home>
2021-04-23 22:21:36 -0400

    make product tile description pane taller

--- resources/views/components/product-tile.blade.php
+++ resources/views/components/product-tile.blade.php
@@ -9,7 +9,7 @@
                 <span class="text-green-300">+{{ $product->margin_percentage }}%</span>
             </span>
         </div>
-        <div class="product-tile-info absolute bg-opacity-50 bg-gray-900 top-3/4 left-0 right-0 text-white pt-4 px-4 h-64 transition-all ease-in-out duration-300">
+        <div class="product-tile-info absolute bg-opacity-50 bg-gray-900 top-3/4 left-0 right-0 text-white pt-4 px-4 h-96 transition-all ease-in-out duration-300">
             <div class="product-title">{{ $product->title }}</div>
             <div class="product-description text-gray-400">{{ $product->excerpt }}</div>
         </div>
